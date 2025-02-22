6c0dc6fdb72161c909bcfa1984bf4a8e9b9ee487
yuseoyeon <kksy103@naver.com>
2025-02-22 15:57:51 +0900

    Upload 20250222

---
+++ 2480.py
@@ -0,0 +1,15 @@
+# a,b,c = input().split(" ")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+
+# if a==b==c:
+#     print(10000+a*1000)
+# elif a==b:
+#     print(1000+a*100)
+# elif b==c:
+#     print(1000+b*100)
+# elif a==c:
+#     print(1000+a*100)
+# elif a!=b!=c:
+#     print
